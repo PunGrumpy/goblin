8c2f94e1df04de9266f97c3ee34b3ccb7b51382b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-06-18 16:09:50 +0700

    fix: name project check sha

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -1,7 +1,7 @@
 package app
 
 const (
-	Version     = "0.1.3"
+	Version     = "0.1.4"
 	Name        = "goblin"
 	Description = "Goblin is a command-line tool for hashing and reversing hashes using Jenkins' one time hash function."
 )
