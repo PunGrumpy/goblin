30ad34b087263b631940dff7c809fb3cf2ccc675
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-04 23:07:23 +0700

    feat(cmd): update pre-run

--- cmd/app.go
+++ cmd/app.go
@@ -1,28 +1,29 @@
 package cmd
 
 import (
+	"github.com/PunGrumpy/goblin/external/logger"
 	"github.com/PunGrumpy/goblin/internal/app"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:           app.Name,
-	Short:         app.Description,
-	Version:       app.Version,
-	SilenceErrors: true,
-	BashCompletionFunction: `
-	__goblin_bash_completion() {
-		local current_word
-		COMPREPLY=()
-		current_word="${COMP_WORDS[COMP_CWORD]}"
-		if [[ ${COMP_CWORD} -eq 1 ]]; then
-			COMPREPLY=( $(compgen -W "hash reverse" -- ${current_word}) )
-		fi
+	Use:              app.Name,
+	Short:            app.Description,
+	Version:          app.Version,
+	SilenceErrors:    true,
+	TraverseChildren: true,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if err := cmd.GenBashCompletionFile(app.Name + ".bash"); err != nil {
+			logger.PrintError("Failed to generate bash completion file: " + err.Error())
+		}
+		if err := cmd.GenZshCompletionFile(app.Name + ".zsh"); err != nil {
+			logger.PrintError("Failed to generate bash completion file: " + err.Error())
 
-		return 0
-
-	} && complete -F __goblin_bash_completion goblin
-	`,
+		}
+		if err := cmd.GenFishCompletionFile(app.Name+".fish", true); err != nil {
+			logger.PrintError("Failed to generate bash completion file: " + err.Error())
+		}
+	},
 }
 
 func Execute() error {
--- internal/app/constants.go
+++ internal/app/constants.go
@@ -1,7 +1,7 @@
 package app
 
 const (
-	Version     = "0.2.0"
+	Version     = "0.2.1"
 	Name        = "goblin"
 	Description = "Goblin is a command-line tool for hashing and reversing hashes using Jenkins' one time hash function."
 )
