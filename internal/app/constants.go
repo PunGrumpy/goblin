891ba7452228446942a2ac154ed2523445d55306
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-27 01:07:15 +0700

    chore(constant): update version

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -1,7 +1,7 @@
 package app
 
 const (
-	Version     = "0.2.6"
+	Version     = "0.2.7"
 	Name        = "goblin"
 	Description = "Goblin is a command-line tool for hashing and reversing hashes using Jenkins' one time hash function."
 )
